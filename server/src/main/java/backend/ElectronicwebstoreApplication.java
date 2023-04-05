ec410b9698595a763932f5dd9d5899520f769c5e
= <=>
2023-04-05 13:25:36 +0700

    init backend project

---
+++ server/src/main/java/backend/ElectronicwebstoreApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicwebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicwebstoreApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/backend/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String>getDefault(){
+        return ResponseEntity.status(200).body("Hello");
+    }
+
+}
