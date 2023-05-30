cb324913ccde272115bccfa49845ef44d67146cb
= <=>
2023-05-30 13:23:37 +0700

    add cart to route

--- client/src/route.js
+++ client/src/route.js
@@ -3,6 +3,7 @@ import App from "./App";
 import ErrorPage from "./components/Error/Error";
 import Login from "./components/Login/Login";
 import Register from './components/Register/Register';
+import Cart from './components/Cart/Cart.js'
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
             {
                 path: '/registry',
                 element: <Register />
+            },
+            {
+                path: '/cart',
+                element: <Cart />
             }
         ]
     }
